b925c1424efa183e019b1601f7846fccde20deff
MilonHossain61 <milon.hossain@northsouth.edu>
2019-08-25 01:20:49 +0600

    connect signup activity to login activity by clicking a button

--- app/src/main/java/com/example/onlineshopping/Signup_Form.java
+++ app/src/main/java/com/example/onlineshopping/Signup_Form.java
@@ -2,13 +2,27 @@ package com.example.onlineshopping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup_Form extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
+
+        button1=(Button) findViewById(R.id.btn1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Signup_Form.this,Login_Form.class);
+
+            }
+        });
     }
 }
