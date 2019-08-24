69988878a52553911b7ac26d9f743fa4ca611878
MilonHossain61 <milon.hossain@northsouth.edu>
2019-08-25 00:00:21 +0600

    create signup_Form activity

--- app/src/main/java/com/example/onlineshopping/Signup_Form.java
+++ app/src/main/java/com/example/onlineshopping/Signup_Form.java
@@ -1,32 +1,14 @@
 package com.example.onlineshopping;
 
-import android.content.Intent;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.onlineshopping.R;
+import android.os.Bundle;
 
 public class Signup_Form extends AppCompatActivity {
 
-
-    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__form);
-
-        button2=(Button) findViewById(R.id.btn3);
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Intent intent=new Intent(Signup_Form.this,Login_Form.class);
-//                startActivity(intent);
-            }
-        });
     }
 }
